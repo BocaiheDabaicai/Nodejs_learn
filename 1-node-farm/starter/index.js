7afbf34b2ce1cbe9742d67ded7a916ffc3168987
DESKTOP-KR3UD5D\NfryCreator <54572905@qq.com>
2023-09-04 17:01:31 +0800

    界面完成、模块化文件、npm、包的安装、监测、卸载、更新、设置脚本、介绍版本号

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const http = require('http')
 const url = require('url')
 
+const slugify = require('slugify')
+const replaceTemplate = require('./modules/replaceTemplate.js')
 /* 初始使用
 const hello = 'Hello World'
 console.log(hello)
@@ -35,45 +37,39 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 })
 console.log('Will read file!')
 */
-const replaceTemplate = (temp, product) => {
 
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%FROM%}/g, product.from)
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%ID%}/g, product.id)
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-
-    return output
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-Product.html`, 'utf-8')
 
-
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
-const server = http.createServer((req, res) => {
-    const pathName = req.url;
+let slugs = dataObj.map(element => slugify(element.productName,{lower:true}))
+console.log(slugs)
 
-    if (pathName === '/' || pathName === '/overview') {
+
+const server = http.createServer((req, res) => {
+    const {query, pathname} = url.parse(req.url, true)
+
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, {'Content-type': 'text/html'});
 
         const cardsHtml = dataObj.map(element => replaceTemplate(tempCard, element))
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
         // console.log(cardsHtml)
 
         res.end(output)
 
-    } else if (pathName === '/product') {
-        res.end('<h1>This is the PRODUCT</h1>')
+    } else if (pathname === '/product') {
+        const product = dataObj[query.id]
+        const output = replaceTemplate(tempProduct,product)
 
-    } else if (pathName === '/api') {
+
+        res.end(output)
+
+    } else if (pathname === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data)
 
