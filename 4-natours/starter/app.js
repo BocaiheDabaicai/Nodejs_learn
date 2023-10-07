bd9e79232252a9d6f15330e462a5c39ff59ba7d3
Sonder <108516200+BocaiheDabaicai@users.noreply.github.com>
2023-10-07 11:15:51 +0800

    路由项目型写法、环境变量配置、格式化与检查工具、第五章完结，第六章 MongoDB、MongoDB简介

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -7,6 +7,12 @@ const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
 // 1. 中间件
+console.log(process.env.NODE_ENV)
+// 开发环境监测
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
 // 添加中间件
 app.use(morgan('dev'));
 app.use(express.json())
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -1,25 +1,20 @@
-const express = require("express");
+const express = require('express');
 // const tourController = require('./../controllers/tourController')
 const {
-    checkID,
-    checkBody,
-    getAllTours,
-    postTour,
-    getIdTour,
-    updateTour,
-    deleteTour
-} = require('./../controllers/tourController.js')
+  checkID,
+  checkBody,
+  getAllTours,
+  postTour,
+  getIdTour,
+  updateTour,
+  deleteTour,
+} = require('./../controllers/tourController.js');
 const router = express.Router();
 
-router.param('id', checkID)
+router.param('id', checkID);
 
-router.route('/')
-    .get(getAllTours)
-    .post(checkBody, postTour)
+router.route('/').get(getAllTours).post(checkBody, postTour);
 
-router.route('/:id')
-    .get(getIdTour)
-    .patch(updateTour)
-    .delete(deleteTour)
+router.route('/:id').get(getIdTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
