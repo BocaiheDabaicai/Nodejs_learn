893c42133a1ee8f205401e4bddc3424f0adf3db5
Sonder <108516200+BocaiheDabaicai@users.noreply.github.com>
2023-10-26 10:23:47 +0800

    更新密码、更新用户数据

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,80 +1,117 @@
-const User = require('../models/userModel')
-const catchAsync = require('../utils/catchAsync')
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getAllUsers = catchAsync(async(req, res) => {
-    const users = await User.find()
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
-    res.status(200).json({
-        status: 'success',
-        results:users.length,
-        data:{
-            users
-        }
-    })
-})
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm)
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400,
+      ),
+    );
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user:updateUser,
+    },
+  });
+});
 
 exports.getIdUser = (req, res) => {
-    console.log(req.params)
-    const tour = tours.find(item => item.id === +req.params.id)
+  console.log(req.params);
+  const tour = tours.find((item) => item.id === +req.params.id);
 
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
 
-    res.status(200).json({
-        status: 'success', data: {
-            tour
-        }
-    })
-}
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
 exports.postUser = (req, res) => {
-    console.log(req.body)
+  console.log(req.body);
 
-    const newId = tours.at(-1).id + 1;
-    const newTour = Object.assign({id: newId}, req.body)
+  const newId = tours.at(-1).id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
 
-    tours.push(newTour);
+  tours.push(newTour);
 
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success', data: {
-                tour: newTour
-            }
-        })
-    })
-
-    // 一次请求中，响应只能出现一次
-    // res.send('Done')
-}
-exports.updateUser = (req, res) => {
-
-    if (+req.params.id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-
-    res.status(200).json({
-        status: 'succes',
-        data: {
-            tour: '<Update></Update>'
-        }
-    })
-}
-exports.deleteUser = (req, res) => {
-    if (+req.params.id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid Id'
-        })
-    }
-
-    res.status(204).json({
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
         status: 'success',
-        data: null
-    })
-}
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+
+  // 一次请求中，响应只能出现一次
+  // res.send('Done')
+};
+exports.updateUser = (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'succes',
+    data: {
+      tour: '<Update></Update>',
+    },
+  });
+};
+exports.deleteUser = (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -1,21 +1,29 @@
-const express = require("express");
-const {getAllUsers, postUser, getIdUser, updateUser, deleteUser} = require('./../controllers/userController')
-const authController = require('../controllers/authController')
+const express = require('express');
+const {
+  getAllUsers,
+  updateMe,
+  postUser,
+  getIdUser,
+  updateUser,
+  deleteUser,
+} = require('./../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup',authController.signup)
-router.post('/login',authController.login)
-router.post('/forgotPassword',authController.forgotPassword)
-router.patch('/resetPassword/:token',authController.resetPassword)
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword,
+);
+router.patch('/updateMe',authController.protect,updateMe)
 
-router.route('/')
-    .get(getAllUsers)
-    .post(postUser)
+router.route('/').get(getAllUsers).post(postUser);
 
-router.route('/:id')
-    .get(getIdUser)
-    .patch(updateUser)
-    .delete(deleteUser)
+router.route('/:id').get(getIdUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
